6838c5dda4940e0faf4dd1bf08f605b423904e3f
Ștefan Cristian Mladin <stefancristianmladin@outlook.com>
2019-06-21 08:18:49 +0300

    Made changes for initializations.

--- springer_metadata_api_access/constraints.py
+++ springer_metadata_api_access/constraints.py
@@ -11,11 +11,18 @@ class Atomic(Constraint, metaclass=ABCMeta):
     pass
 
 class Logical(Constraint, metaclass=ABCMeta):
-    constraint1: Constraint
-    constraint2: Constraint
+    _constraint1: Constraint
+    _constraint2: Constraint
+
+    def __init__(self, constraint1: Constraint, constraint2: Constraint):
+        self._constraint1 = constraint1
+        self._constraint2 = constraint2
 
 class Expression(Constraint):
-    contained: Logical
+    __contained: Logical
+
+    def __init__(self, logical_constraint: Logical):
+        self.__contained = logical_constraint
 
     def _springer_metadata_expression(self):
         pass
@@ -24,8 +31,12 @@ class EqualsField(Enum):
     pass
 
 class Equals(Atomic):
-    field: EqualsField
-    value: str
+    __field: EqualsField
+    __value: str
+
+    def __init__(self, field: EqualsField, value: str):
+        self.__field = field
+        self.__value = value
 
     def _springer_metadata_expression(self):
         pass
@@ -34,8 +45,12 @@ class ContainsField(Enum):
     pass
 
 class Contains(Atomic):
-    field: ContainsField
-    value: str
+    __field: ContainsField
+    __value: str
+
+    def __init__(self, field: ContainsField, value: str):
+        self.__field = field
+        self.__value = value
 
     def _springer_metadata_expression(self):
         pass
@@ -44,7 +59,10 @@ class SortField(Enum):
     pass
 
 class Sort(Atomic):
-    field: SortField
+    __field: SortField
+
+    def __init__(self, field: SortField):
+        self.__field = field
 
     def _springer_metadata_expression(self):
         pass
@@ -56,3 +74,24 @@ class And(Logical):
 class Or(Logical):
     def _springer_metadata_expression(self):
         pass
+
+def EXPR(contained: Logical) -> Constraint:
+    return Expression(contained)
+
+def AND(constraint1: Constraint, constraint2: Constraint) -> Logical:
+    return And(constraint1, constraint2)
+
+def OR(constraint1: Constraint, constraint2: Constraint) -> Logical:
+    return Or(constraint1, constraint2)
+
+def EQ(field: EqualsField, value: str) -> Constraint:
+    return Equals(field, value)
+
+def CONTAINS(field: ContainsField, value: str) -> Constraint:
+    return Contains(field, value)
+
+def IN(value: str, field: ContainsField) -> Constraint:
+    return Contains(field, value)
+
+def SORT(field: SortField) -> Constraint:
+    return Sort(field)
