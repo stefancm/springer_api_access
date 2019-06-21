afbf96738cd46d707ddb0ccf58a58cad7ff9d512
Ștefan Cristian Mladin <stefancristianmladin@outlook.com>
2019-06-21 21:26:39 +0300

    Added fields, checks, and springer query creation.

--- springer_metadata_api_access/constraints.py
+++ springer_metadata_api_access/constraints.py
@@ -1,5 +1,5 @@
 from abc import ABCMeta, abstractmethod
-from enum import Enum
+from enum import Enum, EnumMeta
 
 class Constraint(metaclass=ABCMeta):
     @property
@@ -18,62 +18,106 @@ class Logical(Constraint, metaclass=ABCMeta):
         self._constraint1 = constraint1
         self._constraint2 = constraint2
 
+def _enquote_string( string: str) -> str:
+        if ' ' in string and not(string.startswith('"') and string.endswith('"')):
+            return f'"{string}"'
+        else:
+            return string
+
 class Expression(Constraint):
     __contained: Logical
 
     def __init__(self, logical_constraint: Logical):
+        assert isinstance(logical_constraint, Logical)
         self.__contained = logical_constraint
 
     def _springer_metadata_expression(self):
-        pass
+        return f'({self.__contained._springer_metadata_expression()})'
 
 class And(Logical):
     def _springer_metadata_expression(self):
-        pass
+        return f'{self._constraint1._springer_metadata_expression()} and {self._constraint2._springer_metadata_expression()}'
 
 class Or(Logical):
     def _springer_metadata_expression(self):
-        pass
+        return f'{self._constraint1._springer_metadata_expression()} or {self._constraint2._springer_metadata_expression()}'
 
 class EqualsField(Enum):
-    pass
+    DOI = 'doi'
+    SUBJECT = 'subject'
+    KEYWORD = 'keyword'
+    PUBLICATION = 'pub'
+    YEAR = 'year'
+    ONLINEDATE = 'onlinedate'
+    COUNTRY = 'country'
+    ISBN = 'isbn'
+    ISSN = 'issn'
+    JOURNAL_ID = 'journalid'
+    TOPICAL_COLLECTION = 'topicalcollection'
+    DATE = 'date'
+    ISSUE_TYPE = 'issuetype'
+    ISSUE = 'issue'
+    VOLUME = 'volume'
+    TYPE = 'type'
 
 class ContainsField(Enum):
-    pass
+    EMPTY = 0
+    TITLE = 'title'
+    ORGANIZATION_NAME = 'orgname'
+    JOURNAL = 'journal'
+    BOOK = 'book'
+    NAME = 'name'
 
 class SortField(Enum):
-    pass
+    DATE = 'date'
+    SEQUENCE = 'sequence'
 
 class Equals(Atomic):
     __field: EqualsField
     __value: str
 
     def __init__(self, field: EqualsField, value: str):
+        assert isinstance(field, EqualsField), 'Field should be an EqualsField.'
         self.__field = field
-        self.__value = value
+        self.__value = value if value is str else value.__str__()
 
     def _springer_metadata_expression(self):
-        pass
+        return f'{self.__field.value}:{_enquote_string(self.__value)}'
 
 class Contains(Atomic):
     __field: ContainsField
     __value: str
 
     def __init__(self, field: ContainsField, value: str):
+        assert isinstance(field, ContainsField), 'Field should be a ContainsField.'
         self.__field = field
-        self.__value = value
+        self.__value = value if value is str else value.__str__()
 
     def _springer_metadata_expression(self):
-        pass
+        if(self.__field == ContainsField.EMPTY):
+            return f'{self.__value}'
+
+        return f'{self.__field.value}:{_enquote_string(self.__value)}'
 
 class Sort(Atomic):
     __field: SortField
 
     def __init__(self, field: SortField):
+        assert isinstance(field, SortField), 'Field should be a SortField.'
         self.__field = field
 
     def _springer_metadata_expression(self):
-        pass
+        return f'sort:{self.__field.value}'
+
+class __AbstractEnumMeta(EnumMeta, ABCMeta):
+    pass
+
+class Flag(Atomic, Enum, metaclass=__AbstractEnumMeta):
+    JOURNAL_ONLINE_FIRST = 'journalonlinefirst:true'
+    OPEN_ACCESS = 'openaccess:true'
+
+    def _springer_metadata_expression(self):
+        return self.value
 
 def EXPR(contained: Logical) -> Constraint:
     return Expression(contained)
