2b4a413a46f95da44dd93bc265247aea243a8e8f
Ștefan Cristian Mladin <stefancristianmladin@outlook.com>
2019-06-22 20:13:21 +0300

    Added single article retrieval. Test.ini is ignored as it contains the most secret api key.

--- springer_metadata_api_access/component.py
+++ springer_metadata_api_access/component.py
@@ -1,14 +1,23 @@
-from .constraints import Constraint
+from springer_metadata_api_access.constraints import Constraint
 from typing import Type, TypeVar, List, Generic
 
-def __request(self, url: str) -> dict:
+def _request(url: str) -> dict:
     import urllib.request
-    pass
+    import json
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        r = response.read()
+        return json.loads(r)
 
 T = TypeVar('T')
-def __decorate_dataclass(recordType: Type[T]) -> Type[T]:
+def _decorate_dataclass(recordType: Type[T]) -> Type[T]:
+    import dataclasses
     from marshmallow_dataclass import dataclass, add_schema
-    pass
+
+    if dataclasses.is_dataclass(recordType):
+        return add_schema(recordType)
+    else:
+        return dataclass(recordType)
 
 class MetadataAPISettigs:
     api_key: str
@@ -19,7 +28,7 @@ class MetadataAPIAccess:
     BOOK_ARTICLES_URL_FORMAT = ""
     JOURNAL_ARTICLES_URL_FORMAT = ""
     ARTICLES_URL_FORMAT = ""
-    SINGLE_ARTICLE_URL_FORMAT = ""
+    SINGLE_ARTICLE_URL_FORMAT = "http://api.springernature.com/metadata/json/doi/%s"
 
     __settings: MetadataAPISettigs
 
@@ -28,6 +37,17 @@ class MetadataAPIAccess:
 
     class PagingInformation:
         pass
+
+    def __add_parameters(self, url, query: Constraint = None):
+        result = url + '?'
+
+        result = result + f'api_key={self.__settings.api_key}'
+
+        if(query is not None):
+            assert isinstance(query, Constraint), 'Query should be a constraint.'
+            result = result + '&' + f'q={query._springer_metadata_expression}'
+
+        return result
 
     TRecord = TypeVar('TRecord')
 
@@ -40,6 +60,18 @@ class MetadataAPIAccess:
     def articles(self, query: Constraint = None, record_dataclass: Type[TRecord] = dict) -> List[TRecord]:
         pass
 
-    def article(self, doi: str, record_dataclass: Type[TRecord] = dict) -> List[TRecord]:
-        pass
+    def article(self, doi: str, record_dataclass: Type[TRecord] = dict) -> TRecord:
+        url_string = self.SINGLE_ARTICLE_URL_FORMAT % doi
+        url_string = self.__add_parameters(url_string)
+        result_dict = _request(url_string)
+
+        if not result_dict['records']:
+            return None
+        else:
+            if record_dataclass is not dict:
+                record_marshmallow_dataclass = _decorate_dataclass(record_dataclass)
+                result, _ = record_marshmallow_dataclass.Schema().load(result_dict['records'][0])
+                return result
+            else:
+                return result_dict['records'][0]
 
--- springer_metadata_api_access/test_metadataAPIAccess.py
+++ springer_metadata_api_access/test_metadataAPIAccess.py
@@ -1,7 +1,18 @@
 from unittest import TestCase
+from dataclasses import dataclass, field
+from springer_metadata_api_access.component import MetadataAPIAccess, MetadataAPISettigs
+from typing import Type, TypeVar, List
+
+import configparser
+config = configparser.ConfigParser()
+
+config.read('Test.ini')
+api_key: str = config['General']['ApiKey']
+
 
 
 class TestMetadataAPIAccess(TestCase):
+
     def test_articles_in_book(self):
         self.fail()
 
@@ -11,5 +22,48 @@ class TestMetadataAPIAccess(TestCase):
     def test_articles(self):
         self.fail()
 
+    T = TypeVar('T')
+    def base_arrange_act_test_article(self, recordType: Type[T]) -> T:
+        settings = MetadataAPISettigs()
+        settings.api_key = api_key
+        settings.page_size = 10
+
+        component = MetadataAPIAccess(settings)
+
+
+        doi = '10.1007/978-3-540-74930-1_26'
+
+        if recordType is not None:
+            return component.article(doi, recordType)
+        else:
+            return component.article(doi)
+
     def test_article(self):
-        self.fail()
+
+        with self.subTest('Record type is dataclass'):
+            @dataclass
+            class TestArticleDataClass:
+                title: str
+                abstract: str
+                publication_date: str = field(metadata={'load_from':'publicationDate'})
+            result = self.base_arrange_act_test_article(TestArticleDataClass)
+            self.assertEqual(result.title, 'Semantic Web and Rule Reasoning inside of E-Learning Systems')
+            self.assertEqual(result.abstract, 'SummaryThis paper describes how the Semantic Web and the JenaRules can work together in order to empower e-Learning platform Moodle with rich reports and flexible courses management. We discuss aspects of using Semantic Web technologies in e-learning in general and the JenaRules in particular. The main focus is towards using rules for reasoning on top of existing Moodle content.')
+            self.assertEqual(result.publication_date, '2008-01-01')
+
+        with self.subTest('Record type is non dataclass'):
+            class TestArticleNonDataClass:
+                title: str
+                abstract: str
+                publication_date: str = field(metadata={'load_from': 'publicationDate'})
+            result = self.base_arrange_act_test_article(TestArticleNonDataClass)
+            self.assertEqual(result.title, 'Semantic Web and Rule Reasoning inside of E-Learning Systems')
+            self.assertEqual(result.abstract, 'SummaryThis paper describes how the Semantic Web and the JenaRules can work together in order to empower e-Learning platform Moodle with rich reports and flexible courses management. We discuss aspects of using Semantic Web technologies in e-learning in general and the JenaRules in particular. The main focus is towards using rules for reasoning on top of existing Moodle content.')
+            self.assertEqual(result.publication_date, '2008-01-01')
+
+        with self.subTest('Record type is not provided'):
+            result = self.base_arrange_act_test_article(None)
+            self.assertEqual(result['title'], 'Semantic Web and Rule Reasoning inside of E-Learning Systems')
+            self.assertEqual(result['abstract'], 'SummaryThis paper describes how the Semantic Web and the JenaRules can work together in order to empower e-Learning platform Moodle with rich reports and flexible courses management. We discuss aspects of using Semantic Web technologies in e-learning in general and the JenaRules in particular. The main focus is towards using rules for reasoning on top of existing Moodle content.')
+            self.assertEqual(result['publicationDate'], '2008-01-01')
+            self.assertEqual(result['publicationName'], 'Advances in Intelligent and Distributed Computing')
