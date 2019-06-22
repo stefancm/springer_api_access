7aa5cf812c273edeb1af364f9a717b66c67c921c
Ștefan Cristian Mladin <stefancristianmladin@outlook.com>
2019-06-22 21:38:41 +0300

    Added the rest of the methods for getting calling the metadata api.

--- springer_metadata_api_access/component.py
+++ springer_metadata_api_access/component.py
@@ -4,6 +4,7 @@ from typing import Type, TypeVar, List, Generic
 def _request(url: str) -> dict:
     import urllib.request
     import json
+
     req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as response:
         r = response.read()
@@ -25,9 +26,9 @@ class MetadataAPISettigs:
 
 
 class MetadataAPIAccess:
-    BOOK_ARTICLES_URL_FORMAT = ""
-    JOURNAL_ARTICLES_URL_FORMAT = ""
-    ARTICLES_URL_FORMAT = ""
+    BOOK_ARTICLES_URL_FORMAT = "http://api.springernature.com/metadata/json/isbn/%s"
+    JOURNAL_ARTICLES_URL_FORMAT = "http://api.springernature.com/metadata/json/issn/%s"
+    ARTICLES_URL_FORMAT = "http://api.springernature.com/metadata/json"
     SINGLE_ARTICLE_URL_FORMAT = "http://api.springernature.com/metadata/json/doi/%s"
 
     __settings: MetadataAPISettigs
@@ -43,22 +44,37 @@ class MetadataAPIAccess:
 
         result = result + f'api_key={self.__settings.api_key}'
 
-        if(query is not None):
+        if query is not None:
+            import urllib.request
             assert isinstance(query, Constraint), 'Query should be a constraint.'
-            result = result + '&' + f'q={query._springer_metadata_expression}'
+            escaped_expression = urllib.request.quote(query._springer_metadata_expression())
+            result = result + '&' + f'q={escaped_expression}'
 
         return result
 
     TRecord = TypeVar('TRecord')
+    def __base_articles(self, url_string: str, query: Constraint, record_dataclass: Type[TRecord]) -> List[TRecord]:
+        url_string = self.__add_parameters(url_string, query)
+        result_dict = _request(url_string)
+
+        if not result_dict['records']:
+            return None
+        else:
+            if record_dataclass is not dict:
+                record_marshmallow_dataclass = _decorate_dataclass(record_dataclass)
+                result, _ = record_marshmallow_dataclass.Schema().load(result_dict['records'], many=True)
+                return result
+            else:
+                return result_dict['records']
 
     def articles_in_book(self, isbn: str, query: Constraint = None, record_dataclass: Type[TRecord] = dict) -> List[TRecord]:
-        pass
+        return self.__base_articles(self.BOOK_ARTICLES_URL_FORMAT % isbn, query, record_dataclass)
 
     def articles_in_journal(self, issn: str, query: Constraint = None, record_dataclass: Type[TRecord] = dict) -> List[TRecord]:
-        pass
+        return self.__base_articles(self.JOURNAL_ARTICLES_URL_FORMAT % issn, query, record_dataclass)
 
     def articles(self, query: Constraint = None, record_dataclass: Type[TRecord] = dict) -> List[TRecord]:
-        pass
+        return self.__base_articles(self.ARTICLES_URL_FORMAT, query, record_dataclass)
 
     def article(self, doi: str, record_dataclass: Type[TRecord] = dict) -> TRecord:
         url_string = self.SINGLE_ARTICLE_URL_FORMAT % doi
